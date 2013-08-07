96d6509e0804ed21d69cfc6cb9feafe1fd835e39
brook <brook@github.com>
2013-08-07 16:27:11 +0100

    now you can move around inside the maze

--- main.c
+++ main.c
@@ -6,12 +6,34 @@
 #include "quaternion.h"
 #include "maze.h"
 
+typedef struct {
+  int code;
+  int pressed;
+} input;
+
 int tabbed_out;
+input inputs[64];
+int num_inputs;
 
 maze m;
 
+float xpos, ypos, zpos;
+quaternion q;
+
+GLfloat matrix[16];
+
 void init(void) {
   tabbed_out = 0;
+  
+  num_inputs = 0;
+  inputs[num_inputs++] = (input){SDLK_q,0};
+  inputs[num_inputs++] = (input){SDLK_e,0};
+  inputs[num_inputs++] = (input){SDLK_w,0};
+  inputs[num_inputs++] = (input){SDLK_s,0};
+  inputs[num_inputs++] = (input){SDLK_a,0};
+  inputs[num_inputs++] = (input){SDLK_d,0};
+  
+  //
   
   m.w = 4;
   m.h = 3;
@@ -19,6 +41,9 @@ void init(void) {
   m.cells = malloc(m.w*m.h*m.d*sizeof(cell));
   init_maze(&m);
   generate_maze(&m,0,0,0);
+  
+  xpos = 0; ypos = 0; zpos = 0;
+  q = quaternion_axis_angle(0, 0, 0, 0); 
   
   //
   
@@ -33,7 +58,7 @@ void init(void) {
   glViewport(0, 0, 640, 480);
   
   glMatrixMode(GL_PROJECTION);
-  gluPerspective(60.0, 4.0/3.0, 0.5, 1024.0);
+  gluPerspective(75.0, 4.0/3.0, 0.1, 256.0);
   
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
@@ -93,11 +118,6 @@ void draw_wall(cube_side dir) {
   }
 }
 
-void update(float dt) {
-  //printf("%f\n", dt);
-  t += dt*60;
-}
-
 void draw(void) {
   int x,y,z;
   cube_side d;
@@ -105,10 +125,10 @@ void draw(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
   glLoadIdentity();
-  glTranslatef(0,0,-7);
-  glRotatef(t,0,1,0);
-  glRotatef(t,0,0,0.3);
-  glTranslatef(-(m.w-1)/2.0,-(m.h-1)/2.0,-(m.d-1)/2.0);
+  quaternion_rotation_matrix(q, matrix);
+  glMultMatrixf(matrix);
+  glTranslatef(xpos, ypos, zpos);
+  //glTranslatef(-(m.w-1)/2.0,-(m.h-1)/2.0,-(m.d-1)/2.0);
   
   for(x=0; x<m.w; x++) {
     for(y=0; y<m.h; y++) {
@@ -130,8 +150,84 @@ void draw(void) {
   SDL_GL_SwapBuffers();
 }
 
-void handle_event(SDL_Event event) {
+void update(float dt) {
+  float roll_sensitivity = 2;
+  float move_speed = 2.5;
+  quaternion roll;
   
+  float x,y,z;
+  
+  t += dt*60;
+  
+  if(inputs[0].pressed && !inputs[1].pressed) {
+    roll = quaternion_axis_angle(0, 0, 1, roll_sensitivity*dt);
+    q = quaternion_multiply(roll, q);
+  }
+  if(inputs[1].pressed && !inputs[0].pressed) {
+    roll = quaternion_axis_angle(0, 0, 1, -roll_sensitivity*dt);
+    q = quaternion_multiply(roll, q);
+  }
+  
+  if(inputs[2].pressed && !inputs[3].pressed) {
+    x = 0; y = 0; z = 1;
+    quaternion_rotate_vector(q, &x, &y, &z);
+    xpos += x*move_speed*dt;
+    ypos += y*move_speed*dt;
+    zpos += z*move_speed*dt; 
+  }
+  if(inputs[3].pressed && !inputs[2].pressed) {
+    x = 0; y = 0; z = -1;
+    quaternion_rotate_vector(q, &x, &y, &z);
+    xpos += x*move_speed*dt;
+    ypos += y*move_speed*dt;
+    zpos += z*move_speed*dt; 
+  }
+
+  if(inputs[4].pressed && !inputs[5].pressed) {
+    x = 1; y = 0; z = 0;
+    quaternion_rotate_vector(q, &x, &y, &z);
+    xpos += x*move_speed*dt;
+    ypos += y*move_speed*dt;
+    zpos += z*move_speed*dt; 
+  }
+  if(inputs[5].pressed && !inputs[4].pressed) {
+    x = -1; y = 0; z = 0;
+    quaternion_rotate_vector(q, &x, &y, &z);
+    xpos += x*move_speed*dt;
+    ypos += y*move_speed*dt;
+    zpos += z*move_speed*dt; 
+  }
+}
+
+void handle_event(SDL_Event event) {
+  float motion_sensitivity = 0.005;
+  quaternion pitch, yaw;
+  
+  int i;
+  
+  switch(event.type) {
+  case SDL_MOUSEMOTION:
+    pitch = quaternion_axis_angle(1, 0, 0, event.motion.yrel*motion_sensitivity);
+    yaw = quaternion_axis_angle(0, 1, 0, event.motion.xrel*motion_sensitivity);
+    q = quaternion_multiply(quaternion_multiply(pitch, yaw), q);
+    break;
+  case SDL_KEYDOWN:
+    for(i = 0; i < num_inputs; i++) {
+      if(event.key.keysym.sym == inputs[i].code) {
+	inputs[i].pressed = 1;
+	break;
+      }
+    }
+    break;
+  case SDL_KEYUP:
+    for(i = 0; i < num_inputs; i++) {
+      if(event.key.keysym.sym == inputs[i].code) {
+	inputs[i].pressed = 0;
+	break;
+      }
+    }
+    break;
+  }
 }
 
 int main(int argc, char *argv[]) {
